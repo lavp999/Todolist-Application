6a9a27722a5b1faf96e4039df46ac2f4d67de22a
lavp999 <lavp99@hotmail.com>
2022-11-11 20:31:49 +0000

    casi casi

---
+++ src/js/component/elementoLista.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ElementoLista = (props) => {
+
+	return (
+		<div>
+			{props.lista}
+		</div>
+	);
+};
+
+export default ElementoLista;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,37 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, {useState } from "react";
+import ElementoLista from "./elementoLista.jsx";
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue ] = useState('');
+	const [lista, setlista ] = useState(["numero 1"]);
+
+	function asigna(evento){
+		setInputValue(evento.target.value);
+	}
+
+	function sumaToDo(evento){
+		if (evento.keyCode == 13) {
+			setlista([].concat(lista, [evento.target.value]));
+			setInputValue("");
+		}
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<div className="cajaBlanca">
+				<div className="text-center">
+					<p className="titulo">todos</p>
+				</div>
+				<div className="tarjeta">
+					<div>
+						<input className="cajaInput" type="text" onChange={asigna} value={inputValue} onKeyDown={sumaToDo}/>
+					</div>
+					<div>
+						<ElementoLista lista={lista} />
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
