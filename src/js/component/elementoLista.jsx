84c2e9db47b55e3beb62980f8ca00b2635874cd0
Luis <lavp99@hotmail.com>
2022-11-19 20:19:45 +0100

    hasta poner el botón de borrado alineado

--- src/js/component/elementoLista.jsx
+++ src/js/component/elementoLista.jsx
@@ -7,12 +7,14 @@ const borraElementos = elemento => {
 const listaItems = (miArray) => miArray.map((elemento, index) => {
 									console.log(elemento, index);
 									return (<li >
-											<button className="btn" onClick={borraElementos}>{elemento} <i className="fas fa-trash-alt" /></button>
+												<div className="divLi">
+													<p>{elemento}</p>
+													<button className="btn" onClick={borraElementos}> <i className="fas fa-trash-alt" /></button>
+												</div>
 											</li>
 											)
 								});
-
-
+								
 const ElementoLista = (props) => {
 	return (<ul>
 			{listaItems(props.lista)}
